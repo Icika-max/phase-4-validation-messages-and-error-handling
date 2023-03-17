a09f0d77e7bfd430841e5ea7beb2d1dcc9c78e80
icika-max <icikakennedy@gmail.com>
2023-03-17 06:18:18 +0300

    completed assignment

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,62 +1,169 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import CategoryFilter from "./CategoryFilter";
-import MovieCard from "./MovieCard";
 
-function MovieList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [movies, setMovies] = useState([]);
+function MovieForm() {
+  const [errors, setErrors] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    year: new Date().getFullYear(),
+    length: "0",
+    director: "",
+    description: "",
+    poster_url: "",
+    category: "",
+    discount: false,
+    female_director: false,
+  });
 
-  useEffect(() => {
-    fetch("/movies")
-      .then((r) => r.json())
-      .then((movies) => {
-        setMovies(movies);
-      });
-  }, []);
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/movies", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
+    });
+  }
 
-  const categories = movies
-    .map((movie) => movie.category)
-    .filter(
-      (category, index, categories) => categories.indexOf(category) === index
-    )
-    .sort();
-
-  const displayedMovies = movies.filter(
-    (movie) => selectedCategory === "All" || movie.category === selectedCategory
-  );
+  function handleChange(e) {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setFormData({
+      ...formData,
+      [e.target.id]: value,
+    });
+  }
 
   return (
     <Wrapper>
-      <Sidebar>
-        <CategoryFilter
-          categories={["All", ...categories]}
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
-      </Sidebar>
-      <section>
-        {displayedMovies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </section>
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="year">Year</label>
+          <input
+            type="number"
+            id="year"
+            min="1888"
+            max={new Date().getFullYear()}
+            value={formData.year}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="length">Length</label>
+          <input
+            type="number"
+            id="length"
+            value={formData.length}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="director">Director</label>
+          <input
+            type="text"
+            id="director"
+            value={formData.director}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="poster_url">Poster</label>
+          <input
+            type="text"
+            id="poster_url"
+            value={formData.poster_url}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="category">Category</label>
+          <input
+            type="text"
+            id="category"
+            value={formData.category}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="discount">
+            Discount?
+            <input
+              type="checkbox"
+              id="discount"
+              checked={formData.discount}
+              onChange={handleChange}
+            />
+          </label>
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="female_director">
+            Female Director?
+            <input
+              type="checkbox"
+              id="female_director"
+              checked={formData.female_director}
+              onChange={handleChange}
+            />
+          </label>
+        </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+        <SubmitButton type="submit">Add Movie</SubmitButton>
+      </form>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.main`
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 32px 16px;
-  display: grid;
-  gap: 16px;
-  grid-template-columns: 200px 1fr;
+const Wrapper = styled.section`
+  max-width: 500px;
+  margin: 32px auto;
+  padding: 32px;
 `;
 
-const Sidebar = styled.nav`
-  position: sticky;
-  top: 32px;
-  height: 25vh;
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
 `;
 
-export default MovieList;
+const SubmitButton = styled.button`
+  background: blue;
+  color: yellow;
+  font-weight: bold;
+  font-family: inherit;
+  font-size: 1.2rem;
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
+`;
+
+export default MovieForm;
